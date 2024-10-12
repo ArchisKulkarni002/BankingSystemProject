51815646f0a4fbd65173d9454013145f0ec63d09
ArchisKulkarni002 <archiskulkarni002@gmail.com>
2024-10-12 08:31:29 +0530

    added counter files. updated menus.

--- headers/menus.c
+++ headers/menus.c
@@ -27,3 +27,62 @@ void print_underline(int counter){
     
     printf("\n\n");
 }
+
+// LOGIN SCREENS
+void print_login_prompt(){
+    print_subheading("Login screen, enter you id and password.");
+}           
+void print_login_success(char* role){
+    printf("Logged in as %s.\n", role);
+}
+void print_login_failure(){
+    printf("Log in failed, please try again.\n");
+}          
+void print_logout_message(){
+    printf("Logged out successfully.\n");
+}
+
+// ROLE-WISE MENUS
+void print_customer_menu(){
+    print_heading("Customer Menu");
+    printf("1.View Account Balance \n");
+    printf("2.Deposit Money \n");
+    printf("3.Withdrawl Money \n");
+    printf("4.Transfer Funds \n");
+    printf("5.Apply for a Loan  \n");
+    printf("6.Change Password   \n");
+    printf("7.Adding Feedback    \n");
+    printf("8.View Transaction History    \n");
+    printf("9.Logout\n");
+    printf("10.Exit\n");
+}          
+void print_employee_menu(){
+    print_heading("Employee Menu");
+    printf("1.Add New Customer  \n");
+    printf("2.Modify Customer Details \n");
+    printf("3.Process Loan Applications \n");
+    printf("4.Approve/Reject Loans \n");
+    printf("5.View Assigned Loan Applications  \n");
+    printf("6.View Customer Transactions   \n");
+    printf("7.Change Password    \n");
+    printf("8.Logout\n");
+    printf("9.Exit\n");
+}          
+void print_manager_menu(){
+    print_heading("Manager Menu");
+    printf("1.Activate/Deactivate Customer Accounts  \n");
+    printf("2.Assign Loan Application Processes to Employees \n");
+    printf("3.Review Customer Feedback \n");
+    printf("4.Change Password    \n");
+    printf("5.Logout\n");
+    printf("6.Exit\n");
+}           
+void print_admin_menu(){
+    print_heading("Admin Menu");
+    printf("1.Add New Bank Employee\n");
+    printf("2.Modify Customer/Employee Details \n");
+    printf("3.Manage User Roles  \n");
+    printf("4.Change Password    \n");
+    printf("5.Logout\n");
+    printf("6.Exit\n");
+}             
--- headers/menus.h
+++ headers/menus.h
@@ -5,4 +5,15 @@ void print_heading(char* heading);
 void print_subheading(char* heading);
 void print_underline(int counter);
 
+void print_login_prompt();           
+void print_login_success(char* role);
+void print_login_failure();          
+void print_logout_message();
+
+void print_customer_menu();          
+void print_employee_menu();          
+void print_manager_menu();           
+void print_admin_menu();             
+
+
 #endif // MENUS_H
